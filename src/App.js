bc961981e992cc7c65771d3fddf718d47f29f840
unknown <hannahhtn97@gmail.com>
2022-07-27 13:06:07 -0700

    added temperature, buttons and functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { toBeEmptyDOMElement } from "@testing-library/jest-dom/dist/matchers";
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [temperature, setTemperature] = useState(10);
+  const [color, setColor] = useState("cold");
+
+  const setTempAndColor = (button) => {
+    let newTemp = temperature;
+    if(button === "increase")
+    {
+      setTemperature(temperature + 1);
+      newTemp++;
+    }
+    else
+    {
+      setTemperature(temperature - 1);
+      newTemp--;
+    }
+
+    if(newTemp >= 15)
+    {
+      setColor("hot");
+    }
+    else 
+    {
+      setColor("cold");
+    }
+  }
+
+	return (
+		<div className="app-container">
+			<div className={`temperature + ${color}`}>{temperature}°</div>
+			<div>
+				<button
+					type="button"
+					className="btn btn-light rounded-pill m-2"
+					onClick={() => setTempAndColor("increase")}
+				>
+					+
+				</button>
+				<button
+					type="button"
+					className="btn btn-light rounded-pill"
+					onClick={() => setTempAndColor("decrease")}
+				>
+					-
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
